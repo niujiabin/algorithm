c6d647def4a2665439976de868e942c64cfafd89
Administrator <2931556626@qq.com>
2019-05-13 23:50:26 +0800

    第二天

---
+++ src/main/java/wanghui/链表/第二天-倒数第K个结点.java
@@ -0,0 +1,21 @@
+package Test;
+
+public class 倒数第K个结点 {
+    public ListNode FindKthToTail(ListNode head,int k) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && k>0) {
+            fast = fast.next;
+            k--;
+        }
+        if(k>0) {
+            return null;
+        }else {
+            while(fast!=null) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+            return slow;
+        }
+    }
+}
