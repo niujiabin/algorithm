61f737159b88ed1efd2482d0a6e898fa42601e17
Administrator <1>
2019-06-04 02:26:45 +0800

    x

---
+++ src/main/java/jiabin/leetcode/链表/奇偶链表.java
@@ -0,0 +1,30 @@
+package jiabin.leetcode.链表;
+
+import common.ListNode;
+
+public class 奇偶链表 {
+
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        // head 为奇链表头结点，o 为奇链表尾节点
+        ListNode o = head;
+        // p 为偶链表头结点
+        ListNode p = head.next;
+        // e 为偶链表尾节点
+        ListNode e = p;
+        while (o.next != null && e.next != null) {
+            o.next = e.next;
+            o = o.next;
+            e.next = o.next;
+            e = e.next;
+        }
+        o.next = p;
+        return head;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
