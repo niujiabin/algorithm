2b504ebe22b04fcecbfb1dbafa631c8cabebe871
njbdhr <1>
2019-06-08 14:29:53 +0800

    重排链表

---
+++ src/main/java/jiabin/leetcode/链表/排序链表.java
@@ -0,0 +1,53 @@
+package jiabin.leetcode.链表;
+
+import common.ListNode;
+
+public class 排序链表 {
+
+    public ListNode sortList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        return mergeSort(head);
+    }
+
+    private ListNode mergeSort(ListNode head) {
+        if (head.next == null) {
+            return head;
+        }
+        //找到链表中间节点的方式 套路  记住这块代码
+        ListNode p = head, q = head, pre = null;
+        while (q != null && q.next != null) {
+            pre = q;
+            p = p.next;
+            q = q.next.next;
+        }
+        pre.next = null;
+        ListNode l = mergeSort(head);
+        ListNode r = mergeSort(p);
+        return merge(l, r);
+    }
+
+    private ListNode merge(ListNode l, ListNode r) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode cur = dummyHead;
+        while (l != null && r != null) {
+            if (l.val <= r.val) {
+                cur.next = l;
+                cur = cur.next;
+                l = l.next;
+            } else {
+                cur.next = r;
+                cur = cur.next;
+                r = r.next;
+            }
+        }
+        if (l != null) {
+            cur.next = l;
+        }
+        if (r != null) {
+            cur.next = r;
+        }
+        return dummyHead.next;
+    }
+}
