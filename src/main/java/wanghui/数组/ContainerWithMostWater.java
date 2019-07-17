16602ca273f60bb30b114863e871419e9fcf6547
Administrator <2931556626@qq.com>
2019-07-17 22:50:23 +0800

    444

---
+++ src/main/java/wanghui/数组/ContainerWithMostWater.java
@@ -0,0 +1,20 @@
+package wanghui.数组;
+
+public class ContainerWithMostWater {    public int maxArea(int[] height) {
+    if(height==null || height.length==0){return 0;}
+    int low = 0;
+    int high = height.length-1;
+    int max = Integer.MIN_VALUE;
+    int cur = 0;
+    while(low < high) {
+        cur = (high-low)*Math.min(height[low], height[high]);
+        max = Math.max(cur, max);
+        if(height[low]<height[high]) {
+            low++;
+        }else {
+            high--;
+        }
+    }
+    return max;
+}
+}
