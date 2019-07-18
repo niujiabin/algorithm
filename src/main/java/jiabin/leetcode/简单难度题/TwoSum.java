0ad760e58dd265c437fd186d55cb266f519c9e35
jiabin.niu <jiabin.niu@qunar.com>
2019-07-18 22:00:16 +0800

    两数之和

---
+++ src/main/java/jiabin/leetcode/简单难度题/TwoSum.java
@@ -0,0 +1,42 @@
+package jiabin.leetcode.简单难度题;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ * <p>
+ * 示例:
+ * <p>
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * <p>
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author : jiabin.niu
+ * @date : 2019.07.18 21:48
+ */
+public class TwoSum {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            map.putIfAbsent(nums[i], i);
+            if ((map.get(target - nums[i])) != null) {
+                return new int[]{i, map.get(target - nums[i])};
+            }
+        }
+        return new int[]{};
+    }
+}
